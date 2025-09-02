07a32cad868aab1f726e2714cefeecfc5def8949
KaykyFernandez <kaykyfernandes31@gmail.com>
2025-09-01 21:10:17 -0300

    01/09/2025

---
+++ Pessoa.java
@@ -0,0 +1,11 @@
+public class Pessoa {
+
+        public String saudacao(String saudacao){
+            if(saudacao == "oi"){
+                return "ola tudo em ?";
+            }else{
+                return "sai fora";
+            }
+            }      
+    
+}
---
+++ hello.java
@@ -0,0 +1,10 @@
+
+public class hello {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+        Pessoa kayky = new Pessoa();
+        String teste = kayky.saudacao("oi");
+        System.out.println(teste);
+    }
+    
+}
